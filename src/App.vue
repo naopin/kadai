8bdf0c402bcd1cd407781e13f4569ad313abf9a7
naopin <naopin708@yahoo.co.jp>
2020-06-02 00:09:56 +0900

    確認画面追加

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,7 @@
 <template>
-   <div id="app">
-     <Information></Information>
-     <router-link to = "/ques">Go to Ques</router-link>
-   </div>
+  <div id="app">
+    <router-view> </router-view>
+  </div>
 </template>
-
-<script>
-
-import Information from './components/Information.vue';
-
-export default {
-  
-   name: "",
-   components: {
-      Information
-   },
-    methods: {
-
-    }
-};
-
-</script>
 
 
